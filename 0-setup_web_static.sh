6dde3c4c108f92c47379325c091ba2caa0b3c053
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-03-11 20:00:58 -0700

    last push

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
 apt-get update
 apt-get install -y nginx
